461141bf355e0ec22e110588de7019d5254a9d06
M4t145 <36851132+MatiasOjeda@users.noreply.github.com>
2023-12-28 19:31:24 -0300

    Cambio de mensaje GreetingService.java
    Cambio de mensaje de entrega  por uno personalizado

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		return "Hello, World";
+		return "Esta es una modificación realizada por Matias Ojeda";
 	}
 }
